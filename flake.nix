72df4d025c5371b4f00c339f7d224466e3d3b68d
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2026-05-09 09:10:32 -0300

    Hmmm

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
           name = "nixenv-rebuild";
           runtimeInputs = with pkgs; [ nix ];
           text = ''
-            NIXENV_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/nixenv"
+            NIXENV_CONFIG="''${XDG_CONFIG_HOME:-$HOME/.config}/nixenv"
             nix --extra-experimental-features "nix-command flakes" run "$NIXENV_CONFIG#nixenv-setup"
           '';  
         };
@@ -25,19 +25,19 @@
           name = "nixenv-setup";
           runtimeInputs = with pkgs; [ nix ];
           text = ''
-            XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-            XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
-            XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+            XDG_DATA_HOME="''${XDG_DATA_HOME:-$HOME/.local/share}"
+            XDG_STATE_HOME="''${XDG_STATE_HOME:-$HOME/.local/state}"
+            XDG_CONFIG_HOME="''${XDG_CONFIG_HOME:-$HOME/.config}"
 
             NIXENV_CONFIG="$XDG_CONFIG_HOME/nixenv"
             NIXENV_STATE="$XDG_STATE_HOME/nixenv"
 
             nix --extra-experimental-features "nix-command flakes" build "$NIXENV_CONFIG" -o "$NIXENV_STATE"
 
-            echo fish_add_path --global "$NIXENV_STATE/bin" > "$XDG_CONFIG_HOME/fish/conf.d/nixenv.fish"
-            echo set --export --global --prepend --path MANPATH "$NIXENV_STATE/share/man" /usr/share/man >> "$XDG_CONFIG_HOME/fish/conf.d/nixenv.fish"
+            # echo fish_add_path --global "$NIXENV_STATE/bin" > "$XDG_CONFIG_HOME/fish/conf.d/nixenv.fish"
+            # echo set --export --global --prepend --path MANPATH "$NIXENV_STATE/share/man" /usr/share/man >> "$XDG_CONFIG_HOME/fish/conf.d/nixenv.fish"
 
-            ln -s -n -f "$NIXENV_STATE/share/fish/vendor_completions.d" "$XDG_DATA_HOME/fish/vendor_completions.d" 
+            # ln -s -n -f "$NIXENV_STATE/share/fish/vendor_completions.d" "$XDG_DATA_HOME/fish/vendor_completions.d" 
           '';
         };
 
